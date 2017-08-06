ea2581b85a76009355b2a8da81c07682f34947b9
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-06 15:47:55 -0400

    Add columns to Restaurant class

--- database_setup.py
+++ database_setup.py
@@ -5,14 +5,16 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 
-Base = declarative_base
-
 class Restaurant(Base):
 	__tablename__ = 'restaurant'
+	name = Column(
+		String(80), nullable = False)
+	id = Column(
+		Integer, primary key = True)
 
 class MenuItem(Base):
 	__tablename__ = 'menu_item'
 
-
+Base = declarative_base
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
