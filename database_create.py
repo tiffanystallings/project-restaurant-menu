f042adc6f3cce6e82831dad443e8e736e1ce6001
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-07 21:13:31 -0400

    Add database_create
    This script will populate tables

---
+++ database_create.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+
+session = DBSession()
+
+pizzaPalace = Restaurant(name = "Pizza Palace")
+
+session.add(pizzaPalace)
+session.commit()
