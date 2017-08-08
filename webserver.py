1f744bb75d8e913027a3a329630fdbc81c20ee3d
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-08 15:25:03 -0400

    Add webserver handler
    Takes a simple get request to test the server.

--- webserver.py
+++ webserver.py
@@ -1,6 +1,24 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
+class WebserverHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>Hello!</body></html>"
+				self.wfile.write(output)
+				print output
+				return
+
+		except IOError:
+			self.send_error(404, "File Not Found %s" % self.path)
+
+
 def main():
 	try:
 		port = 8080
