db23dc3faf7e21cfd3a53f09401e874109cf7d34
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-08 16:55:38 -0400

    Add post requests with form for testing

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 
 class WebserverHandler(BaseHTTPRequestHandler):
@@ -10,7 +11,13 @@ class WebserverHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 
 				output = ""
-				output += "<html><body>Hello!</body></html>"
+				output += "<html><body>Hello!"
+				output += "<form method='POST' enctype='multipart/form-data'\
+					action='/hello'><h2>What would you like me to say?</h2>\
+					<input name='message' type='text'><input type='submit'\
+					value='Submit'></form>"
+				output += "</body></html>"
+
 				self.wfile.write(output)
 				print output
 				return
@@ -21,13 +28,47 @@ class WebserverHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 
 				output = ""
-				output += "<html><body>&#161 Hola !</body></html>"
+				output += "<html><body>&#161 Hola !"
+				output += "<form method='POST' enctype='multipart/form-data'\
+					action='/hello'><h2>What would you like me to say?</h2>\
+					<input name='message' type='text'><input type='submit'\
+					value='Submit'></form>"
+				output += "</body></html>"
+
 				self.wfile.write(output)
 				print output
 				return
 
 		except IOError:
 			self.send_error(404, "File Not Found %s" % self.path)
+
+
+	def do_POST(self):
+		try:
+			self.send_response(301)
+			self.end_headers()
+
+			ctype, pdict = cgi.parse_header(
+				self.headers.getheader('content-type'))
+			if ctype == 'multipart/form-data':
+				fields = cgi.parse_multipart(self.rfile, pdict)
+				messagecontent = fields.get('message')
+
+			output = ""
+			output += "<html><body>"
+			output += "<h2>Okay, how about this:</h2>"
+			output += "<h1>%s</h1>" % messagecontent[0]
+			output += "<form method='POST' enctype='multipart/form-data'\
+				action='/hello'><h2>What would you like me to say?</h2>\
+				<input name='message' type='text'><input type='submit'\
+				value='Submit'></form>"
+			output += "</body></html>"
+
+			self.wfile.write(output)
+			print output
+
+		except:
+			pass
 
 
 def main():
