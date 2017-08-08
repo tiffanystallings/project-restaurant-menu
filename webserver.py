a58f232db49a20a17656d22a79f5b9787d51ae15
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-08 15:19:00 -0400

    Add main method
    Starts the web server, and detects keyboard interrupts.

--- webserver.py
+++ webserver.py
@@ -1,1 +1,18 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+def main():
+	try:
+		port = 8080
+		server = HTTPServer(('', port), webserverHandler)
+		print ("Web server is running on port %s" % port)
+		server.serve_forever()
+
+	except KeyboardInterrupt:
+		print ("Keyboard Interrupt detected. \
+			Web server shutting down."
+		server.socket.close()
+
+
+if __name__ == '__main__':
+	main()
