407662c321a948d99c60bbb0cab8eafa7a25659b
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-09 14:24:49 -0400

    Fix bug in database setup
    MenuItem name was set as an integer, and id was left off.

--- database_setup.py
+++ database_setup.py
@@ -16,7 +16,8 @@ class Restaurant(Base):
 
 class MenuItem(Base):
 	__tablename__ = 'menu_item'
-	name = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
 	course = Column(String(250))
 	description = Column(String(250))
 	price = Column(String(8))
