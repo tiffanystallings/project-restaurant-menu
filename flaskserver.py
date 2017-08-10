4f708324ea4bd6983e18d2c7b577724d7588dfce
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-09 21:12:28 -0400

    Add RESTful API functionality

--- flaskserver.py
+++ flaskserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -91,6 +91,26 @@ def deleteMenuItem(restaurant_id, menu_id):
 			restaurant_id = restaurant_id))
 
 	return render_template('delete_item.html', item=item)
+
+
+@app.route('/restaurants/<int:restaurant_id>/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(
+		id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id =
+		restaurant_id).all()
+
+	return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+	restaurant = session.query(Restaurant).filter_by(
+		id=restaurant_id).one()
+	item = session.query(MenuItem).filter_by(id=menu_id).one()
+
+	return jsonify(MenuItems=[items.serialize])
+
 
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
