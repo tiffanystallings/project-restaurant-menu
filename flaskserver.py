67bab6effff552e32fd9713323706b445987192c
Tiffany <tiffany.c.stallings@gmail.com>
2017-08-16 13:27:11 -0400

    Import flask session
    To create user login session

--- flaskserver.py
+++ flaskserver.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import session as login_session
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
