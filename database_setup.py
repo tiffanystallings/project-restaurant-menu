21095d498148e0434e8a84c469f1d2c3b07139e5
Tiffany <tiffany.c.stallings@gmail.com>
2017-09-03 13:50:55 -0400

    Format database_setup for pep8

--- database_setup.py
+++ database_setup.py
@@ -9,50 +9,50 @@ Base = declarative_base()
 
 
 class User(Base):
-	__tablename__ = 'user'
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	email = Column(String(250))
-	picture = Column(String(250))
+    __tablename__ = 'user'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    picture = Column(String(250))
 
-	
+
 class Restaurant(Base):
-	__tablename__ = 'restaurant'
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    __tablename__ = 'restaurant'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		return {
-			'name': self.name,
-			'id': self.id
-		}
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id
+        }
 
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	course = Column(String(250))
-	description = Column(String(250))
-	price = Column(String(8))
-	restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-	restaurant = relationship(Restaurant)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		return {
-			'id': self.id,
-			'name': self.name,
-			'description': self.description,
-			'price': self.price,
-			'course': self.course,
-			'restaurant_id': self.restaurant_id
-		}
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'course': self.course,
+            'restaurant_id': self.restaurant_id
+        }
 
 engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 Base.metadata.create_all(engine)
